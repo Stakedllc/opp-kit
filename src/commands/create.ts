22270c7036fa3ee027fcffeea2cdb52d8ed16613
Nick Paterno <nick@staked.us>
2020-12-08 16:20:15 -0500

    feat: remove native git dep in create

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,15 +1,17 @@
 import {Command, flags} from '@oclif/command';
 import { CONSTANTS } from "../utils";
+import { spawn } from 'child_process';
+import {Clone} from "nodegit";
 
 export default class Create extends Command {
-  static description = 'Create new opportunities.'
+  static description = 'Create new opportunity project.'
 
   static flags = {
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
     // name: flags.string({char: 'n', description: 'name to print', required: true}),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    verbose: flags.boolean({char: 'v'}),
   }
 
   static args = [{name: 'opportunityName'}]
@@ -18,64 +20,41 @@ export default class Create extends Command {
     const {args, flags} = this.parse(Create)
 
     if (!args.opportunityName) {
-      this.log("Missing opportunity name argument.");
+      this.log("\nMissing opportunity name argument.");
       return;
     }
 
-    const { spawn } = require('child_process');
-    await this.git(spawn, args.opportunityName);
-    await this.install(spawn, args.opportunityName);
+    // TODO check for existing directory
+    await this.git(args.opportunityName);
+    await this.install(args.opportunityName, flags.verbose);
 
     this.log();
   }
 
-  async git(spawn:any, opportunityName:string){
-    return new Promise((resolve, reject) => {
-      const git = spawn('git', ['clone', CONSTANTS.template_repo, opportunityName], {cwd: process.cwd()});
-  
-      git.stdout.on('data', (data:any) => {
-        console.log(`stdout: ${data}`);
-      });
-  
-      git.stderr.on('data', (data:any) => {
-        // console.error(`stderr: ${data}`);
-        // reject();
-      });
-  
-      git.on('close', (code:any) => {
-        if (code) {
-          console.log(`child process exited with code ${code}`);
-          reject();
-        } else {
-          this.log(`\nSuccessfully created opportunity: ${opportunityName}\n`);
-          resolve();
-        }
-      });
-    });
+  async git(opportunityName:string) {
+    this.log(`\nCloning repo: ${CONSTANTS.repo.template}\n`);
+    await Clone.clone(CONSTANTS.repo.template, opportunityName)
+    this.log(`\nSuccessfully created new opportunity: ${opportunityName}\n`);
   }
   
-  async install(spawn: any, opportunityName: string) {
-    this.log("Installing dependencies...")
+  async install(opportunityName: string, verbose = false) {
+    this.log("Installing project dependencies...")
+    const writer = (pipeline:any) => (data:any) => {if (verbose) pipeline.write(data)};
     return new Promise((resolve, reject) => {
       const { join } = require("path");
       const install = spawn('npm', ['i'], {
         cwd: join(process.cwd(), opportunityName)
       });
 
-      install.stdout.on('data', (data:any) => {
-        process.stdout.write(data);
-      });
-    
-      install.stderr.on('data', (data:any) => {
-        process.stderr.write(data);
-      });
+      install.stdout.on('data', writer(process.stdout));
+      install.stderr.on('data', writer(process.stderr));
     
       install.on('close', (code:any) => {
         if (code) {
           console.log(`child process exited with code ${code}`);
           reject(code);
         } else {
-          this.log(`\nSuccessfully installed dependencies: ${opportunityName}\n`);
+          this.log(`\nSuccessfully installed dependencies for ${opportunityName} opportunity.\n`);
           resolve();
         }
       });
