72783c6ff068d01289b62e6bf446311eee6d97e3
Nick Paterno <nick@staked.us>
2020-12-10 14:06:23 -0500

    feat: fix casing bug

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -3,12 +3,9 @@ const changeCase =
 
 export default {
     case: {
-        camel: changeCase(
-            (word:string, index:number) => index ? word.toUpperCase() : word.toLowerCase()
-        ),
-        kebab: changeCase(
-            (word:string, index: number) => index ? '_' : '' + word.toLowerCase()
-        ),
-        pascal: changeCase( (word:string) => word.toUpperCase() )
+        camel:  changeCase( (word:string, index:number) => index ? word.toUpperCase() : word.toLowerCase() ),
+        snake:  changeCase( (word:string, index: number) => (index ? '_' : '') + word.toLowerCase() ),
+        pascal: changeCase( (word:string) => word.toUpperCase() ),
+        kebab:  changeCase( (word: string, index: number) => (index ? '-' : '') + word.toLowerCase() )
     }
 }
