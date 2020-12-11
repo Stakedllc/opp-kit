2d4df1ac22bfe9a12180b628a20e25a0552708ed
Nick Paterno <nick@staked.us>
2020-12-11 09:02:54 -0500

    feat: refactor and customize

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,7 +1,9 @@
 import {Command, flags} from '@oclif/command';
 import { join } from 'path';
-import { existsSync } from "fs";
+import { existsSync, writeFile } from "fs";
 import { MISC } from "../utils";
+import https = require('https');
+import CONST from '../utils/constants';
 
 const SOL = '.sol';
 
@@ -10,7 +12,6 @@ export default class Add extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
   }
 
@@ -24,18 +25,74 @@ export default class Add extends Command {
       return;
     }
 
-    // TODO: Assert opportunity.yaml exists
-    const temp = (args.file as string).endsWith(SOL) ? '' : SOL;
-    this.log(temp);
-    const filename = args.file + temp;
-    this.log(filename);
     // TODO: Remove `nicktest`
-    const contractDir = join(process.cwd(), 'nicktest', 'contracts', filename);
-    this.log(contractDir);
-    const exists = existsSync(contractDir);
-    this.log(`${exists}`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const cwd = join(process.cwd(), "nickTest");
+
+    if ( !this.workspaceAssertion(cwd) ) {
+      // Assert opportunity.yaml exists
+      return;
     }
+
+    const FILETYPE = (args.file as string).endsWith(SOL) ? '' : SOL;
+    const filename = MISC.case.pascal(args.file) + FILETYPE;
+    const contractDir = join(cwd, 'contracts');
+    const contractPath = join(contractDir, filename);
+
+    if(this.contractExists(contractPath, flags.force, filename)) {
+      // Assert contract does not exist and --force is false
+      return;
+    }
+    
+    const file = await this.download();
+
+    const output = this.customize(file, args.file);
+
+    await this.writeFile(contractPath, output);
+
+  }
+
+  private contractExists(path:string, force:boolean, name:string){
+    const existsOrForce = existsSync(path) && !force;
+    if(existsOrForce) {
+      this.log([`${name} already exists. Please choose another name or rerun with `,
+                `-f or --force flag to overwrite existing opportunity.`].join(''))
+    }
+    return existsOrForce;
+  }
+
+  private workspaceAssertion(cwd: string) {
+    const inWorkspace = existsSync(join(cwd, "opportunity.yaml")) ;
+    if ( !inWorkspace ) {
+      // Assert opportunity.yaml exists
+      this.log(["File: 'opportunity.yaml' not found. Please navigate to",
+                "the root directory of a valid Opportunity workspace"].join(' '));
+    }
+    return inWorkspace;
+  }
+
+  private writeFile(contractPath: string, output: string) : Promise<void> {
+    return new Promise((resolve, reject) => {
+      writeFile(contractPath, output, (err) => {
+        if (err) reject(err);
+        resolve(void 0);
+      });
+    });
+  }
+
+  private customize(contract: string, name: string) {
+    const pascalName = MISC.case.pascal(name);
+    const camelName = MISC.case.camel(name);
+    return  contract.replace(/(MyOpportunity)/g, pascalName)
+                    .replace(/(myOpportunity)/g, camelName);
+  }
+
+  private download():Promise<string> {
+    return new Promise((resolve, reject) => {
+      https.get(CONST.repo.opportunity.raw, (res) => {
+        const output: string[] = [];
+        res.on('data', (data:any) => output.push(data.toString()))
+           .on('end', () => resolve(output.join('')));
+      }).on('error', reject);
+    });
   }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,13 @@
-export default {
+const repo_url = 'https://github.com/Stakedllc/opportunity-template';
+const CONST = {
     repo : {
-        template: 'https://github.com/Stakedllc/opportunity-template.git'
+        url: repo_url,
+        template: `${repo_url}.git`,
+        opportunity: {
+            blob: `${repo_url}/blob/master/contracts/MyOpportunity.sol`,
+            raw: 'https://raw.githubusercontent.com/Stakedllc/opportunity-template/master/contracts/MyOpportunity.sol'
+        }
     }
 }
+
+export default CONST;
